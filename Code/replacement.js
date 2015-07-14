c431e4829692eb1eda719b52f87c773debb7ad3c
Matthew <bugsby.carlin@gmail.com>
2015-07-14 11:54:39 -0700

    Moving files, small adjustments

--- Code/replacement.js
+++ Code/replacement.js
@@ -3,9 +3,25 @@
 // (It's pretty awesome, I just wanted a more configurable version)
 (function()
 {
+  replacement_list = [];
+
+  chrome.storage.local.get({
+    replacements: 'Force:Horse',
+  }, function(items) {
+    option_list = items.replacements.split("\n");
+    for(var i = 0; i < option_list.length; i++)
+    {
+      replacement_list.push(option_list[i].split(":"));
+    }
+
+    var total_replacements = walk_dom(document.body);
+    console.log("Replaced %d", total_replacements);
+  });
+
   function walk_dom(node) 
   {
     var child, next;
+    var total_replacements = 0;
 
     switch ( node.nodeType )  
     {
@@ -16,22 +32,24 @@
         while ( child ) 
         {
           next = child.nextSibling;
-          walk_dom(child);
+          total_replacements += walk_dom(child);
           child = next;
         }
         break;
       case 3: // Text node
         if(node.parentElement.tagName.toLowerCase() != "script") {
-            make_replacements(node);
+            total_replacements += make_replacements(node);
         }
         break;
     }
+
+    return total_replacements;
   }
 
   function make_replacements(textNode)
   {
-    console.log(replacement_list);
     var value = textNode.nodeValue;
+    var total_replacements = 0;
 
     for(var i = 0; i < replacement_list.length; i++)
     {
@@ -47,10 +65,13 @@
         {
           return matchCase(replacement, match);
         });
+        total_replacements += 1;
       }
     }
 
     textNode.nodeValue = value;
+
+    return total_replacements;
   }
 
   // http://stackoverflow.com/questions/17264639/replace-text-but-keep-case
@@ -75,19 +96,5 @@
 
     return result;
   }
-
-  replacement_list = [];
-
-  chrome.storage.local.get({
-    replacements: 'Force:Horse',
-  }, function(items) {
-    option_list = items.replacements.split("\n");
-    for(var i = 0; i < option_list.length; i++)
-    {
-      replacement_list.push(option_list[i].split(":"));
-    }
-
-    walk_dom(document.body);
-  });
   
 })();
